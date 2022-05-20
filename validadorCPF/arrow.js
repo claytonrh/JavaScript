9958df646501cbc5ef4d290263b0e8ba932a2364
Clayton Salvador <43305715+claytonrh@users.noreply.github.com>
2022-05-20 14:39:18 -0300

    Add files via upload

---
+++ validadorCPF/arrow.js
@@ -0,0 +1,18 @@
+function soma (x, y) {
+    return x + y;
+}
+console.log(soma(5, 4));
+
+const multiplicacao = function(x, y) {
+    return x * y;
+}
+console.log(multiplicacao(5, 4)); 
+
+// com arrow
+const dividir = (x, y) => { 
+    return x / y;
+}
+console.log(dividir(10, 2));
+
+const objeto = () => ({nome: 'CsS', sobrenome: 'sSalvador' });
+
